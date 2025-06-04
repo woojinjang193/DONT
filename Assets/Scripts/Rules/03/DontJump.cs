60a7b17f6d936ccf6d52281717d271cf204c7bd4
woojinjang193 <ryanjang193@gmail.com>
2025-06-04 11:52:25 +0900

    [feat] 스테이지 3 힌트 추가

--- Assets/Scripts/Rules/03/DontJump.cs
+++ Assets/Scripts/Rules/03/DontJump.cs
@@ -7,8 +7,14 @@ public class DontJump : MonoBehaviour, IGameRule
 {
     [SerializeField] private GameObject menu;
     [SerializeField] private GameObject secretFloor;
-    [SerializeField] private Transform monster;
+  
+
+    [SerializeField] private GameObject hint1;
+    [SerializeField] private GameObject hint2;
     private bool jumped = false;
+    private float timer = 0;
+    private bool isHint1Showed = false;
+    private bool isHint2Showed = false;
 
 
     private void Start()
@@ -41,6 +47,24 @@ public class DontJump : MonoBehaviour, IGameRule
         else
         {
             secretFloor.SetActive(false);
+        }
+
+        // 시간지나면 힌트줌
+        timer += Time.deltaTime;
+
+        if(timer>60f && !isHint1Showed)
+        {
+            hint1.SetActive(true);
+            Debug.Log("hint1 출력");
+            isHint1Showed = true;
+        }
+
+
+        if (timer > 120f && !isHint2Showed)
+        {
+            hint2.SetActive(true);
+            Debug.Log("hint2 출력");
+            isHint2Showed = true;
         }
     }
 
